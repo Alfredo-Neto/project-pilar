c0ec2d16783aa400c2ad998626d811d27dd7738e
Alfredo Neto <alfredoneto934@gmail.com>
2023-07-05 02:16:41 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <RouterView />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+%max-size {
+  width: 100%;
+  height: 100%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html,
+body {
+  @extend %max-size;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.app {
+  @extend %max-size;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Banner />
+  </div>
+</template>
+
+<script>
+import Banner from "@/components/Banner";
+
+export default {
+  name: "Home",
+  components: { Banner },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
