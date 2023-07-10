fef3c2cf63ede80573ee4195ef967bb8617461bd
Alfredo Neto <alfredoneto934@gmail.com>
2023-07-10 18:40:36 -0300

    refactor: refactoring storage

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,17 +1,28 @@
 const { stringify, parse } = JSON;
 
-export const setStorage = (key, value, { format } = {}) => {
+export const setStorage = (key, value, options = {}) => {
   if (!window) return;
+
+  const { format } = options;
   window.localStorage.setItem(key, format ? stringify(value) : value);
 };
 
-export const getStorage = (key, { format } = {}) => {
+export const getStorage = (key, options = {}) => {
   if (!window) return;
-  const value = window.localStorage.getItem(key);
-  return format ? parse(window.localStorage.getItem(key)) : value;
+
+  const { format } = options;
+  if (window.localStorage.getItem(key)) {
+    try {
+      const value = window.localStorage.getItem(key);
+      return format ? parse(value) : value;
+    } catch (error) {
+      console.error(`Error parsing stored value for key ${key}: `, error);
+      removeStorage(key);
+    }
+  }
 };
 
 export const removeStorage = (key) => {
   if (!window) return;
-  return parse(window.localStorage.removeItem(key));
+  window.localStorage.removeItem(key);
 };
